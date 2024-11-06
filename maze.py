66a81b4a811a871d2fcbf12001122390f50c2fde
CADU110 <cadumotaalves@gmail.com>
2024-11-06 09:20:31 -0300

    Adicionando DFS e flags
    Co-authored-by: Hugo Queiroz <hugoq2004@gmail.com>

--- maze.py
+++ maze.py
@@ -4,21 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 
-def eh_preto(pixel):
-    return [0, 0, 0] == pixel
-
 def eh_branco(pixel):
     return [255, 255, 255] == pixel
-
-def eh_vermelho(pixel):
-    return [255, 0, 0] == pixel
 
 def eh_verde(pixel):
     return pixel == [0, 255, 0]
 
 
+def mostrar_caminho(adj, pais, start, tam):
+    key = start[0] * tam + start[1]
+
+    while key != tam:
+        idx = pais[key]
+
+        adj[idx[0]][idx[1]] = [204, 153, 0]
+
+        key = idx[0] * tam + idx[1]
+
+        plt.clf()
+
+        plt.imshow(adj)
+        plt.axis('off')
+        plt.pause(0.005)
+    
+    
+
 def bfs(adj):
-    q = deque();
+    q = deque()
     pais = dict()
     curr = []
 
@@ -27,7 +39,7 @@ def bfs(adj):
     q.append([1,0])
 
     while q:
-        curr = q.popleft()
+        curr = q.pop()
         
         el = adj[curr[0]][curr[1]]
 
@@ -37,19 +49,17 @@ def bfs(adj):
 
         plt.imshow(adj)
         plt.axis('off')
-        # plt.pause(0.005)
         plt.pause(0.00005)
 
         for i in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
             res = [x + y for x, y in zip(curr, i)]
+            
             el = adj[res[0]][res[1]]
 
             if eh_verde([el[0], el[1], el[2]]):
-                print(res, curr)
                 pais[res[0] * tam + res[1]] = curr
-                curr = res
-                q.clear()
-                break
+                mostrar_caminho(adj, pais, res, tam)
+                return
 
             if res[0] < len(adj) and res[0] > 0:
                 pass
@@ -65,24 +75,62 @@ def bfs(adj):
                 pais[res[0] * tam + res[1]] = curr
                 q.append(res)
 
-    key = curr[0] * tam + curr[1]
 
-    while key != tam:
-        idx = pais[key]
 
-        adj[idx[0]][idx[1]] = [204, 153, 0]
+def dfs(adj):
+    q = deque()
+    pais = dict()
+    curr = [1,0]
 
-        key = idx[0] * tam + idx[1]
+    tam = len(adj[0])
+    
+    q.append([1,0])
+    adj[curr[0]][curr[1]] = [255, 0, 0]
+
+
+    while q:
+        
+        el = adj[curr[0]][curr[1]]
 
         plt.clf()
 
         plt.imshow(adj)
         plt.axis('off')
-        plt.pause(0.005)
+        plt.pause(0.00005)
+
+        for i in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+            res = [x + y for x, y in zip(curr, i)]
+            el = adj[res[0]][res[1]]
+
+            if eh_verde([el[0], el[1], el[2]]):
+                pais[res[0] * tam + res[1]] = curr
+                mostrar_caminho(adj, pais, res, tam)
+                return
+
+            if res[0] < len(adj) and res[0] > 0:
+                pass
+            else:
+                continue
+
+            if res[1] < len(adj[0]) and res[1] > 0:
+                pass
+            else:
+                continue
+
+            if eh_branco([el[0], el[1], el[2]]):
+                pais[res[0] * tam + res[1]] = curr
+                adj[curr[0]][curr[1]] = [255, 0, 0]
+                curr = res
+                q.append(res)
+                break
+        else:
+            curr = q.pop()
+            adj[curr[0], curr[1]] = [34, 90, 34]
+
 
 def main():
-    if len(sys.argv) < 2:
-        print("Uso: python maze.py maze.png")
+    if len(sys.argv) < 3:
+        print("Uso: python maze.py maze.png algoritmo")
         sys.exit(1)
 
     image = Image.open(sys.argv[1]).convert('RGB')
@@ -97,11 +145,16 @@ def main():
     plt.imshow(image_array)
     plt.axis('off')
 
-    bfs(image_array)
-    print("bla")
+    if sys.argv[2] == "dfs":
+        dfs(image_array)
+    elif sys.argv[2] == "bfs":
+        bfs(image_array)
+    else:
+        print("algoritmo deve ser bfs ou dfs")
 
     plt.ioff()
     plt.show()
 
 if __name__ == "__main__":
     main()
+
