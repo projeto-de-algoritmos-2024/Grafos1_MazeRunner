c21ff7fe62014a13b02c94d99443dcd92308623b
melohugo <hugoq2004@gmail.com>
2024-11-08 09:14:19 -0300

    Adiciona personalização do menu de gerar e solucionar labirinto
    Co-authored-by: Carlos Alves <cadumotaalves@gmail.com>

--- frontend/mazerunner/src/components/Controls.js
+++ frontend/mazerunner/src/components/Controls.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 
 function Controls({ onGenerateMaze, onSolveMaze }) {
-    // Estados para armazenar as entradas do usuário
     const [largura, setLargura] = useState('');
     const [altura, setAltura] = useState('');
     const [paredes, setParedes] = useState('');
     const [algorithm, setAlgorithm] = useState('bfs'); // Padrão para BFS
 
-    // Função de clique para gerar o labirinto com as informações do usuário
     const handleGenerateMazeClick = () => {
         onGenerateMaze({
             largura: parseInt(largura),
@@ -16,52 +14,55 @@ function Controls({ onGenerateMaze, onSolveMaze }) {
         });
     };
 
-    // Função de clique para resolver o labirinto com o algoritmo selecionado
     const handleSolveMazeClick = () => {
         onSolveMaze(algorithm);
     };
 
     return (
-        <div>
-            <h3>Gerar Labirinto</h3>
-            <label>
-                Largura:
-                <input
-                    type="number"
-                    value={largura}
-                    onChange={(e) => setLargura(e.target.value)}
-                    placeholder="Digite a largura"
-                />
-            </label>
-            <label>
-                Altura:
-                <input
-                    type="number"
-                    value={altura}
-                    onChange={(e) => setAltura(e.target.value)}
-                    placeholder="Digite a altura"
-                />
-            </label>
-            <label>
-                Paredes:
-                <input
-                    type="number"
-                    value={paredes}
-                    onChange={(e) => setParedes(e.target.value)}
-                    placeholder="Número de paredes para remover"
-                />
-            </label>
-            <button onClick={handleGenerateMazeClick}>Gerar Labirinto</button>
+        <div className="controls-wrapper">
+            <div className="controls-container">
+                <h3>Gerar Labirinto</h3>
+                <label>
+                    Largura:
+                    <input
+                        type="number"
+                        value={largura}
+                        onChange={(e) => setLargura(e.target.value)}
+                        placeholder="Digite a largura"
+                    />
+                </label>
+                <label>
+                    Altura:
+                    <input
+                        type="number"
+                        value={altura}
+                        onChange={(e) => setAltura(e.target.value)}
+                        placeholder="Digite a altura"
+                    />
+                </label>
+                <label>
+                    Paredes:
+                    <input
+                        type="number"
+                        value={paredes}
+                        onChange={(e) => setParedes(e.target.value)}
+                        placeholder="Número de paredes para remover"
+                    />
+                </label>
+                <button onClick={handleGenerateMazeClick}>Gerar Labirinto</button>
+            </div>
 
-            <h3>Solucionar Labirinto</h3>
-            <label>
-                Algoritmo:
-                <select value={algorithm} onChange={(e) => setAlgorithm(e.target.value)}>
-                    <option value="bfs">BFS</option>
-                    <option value="dfs">DFS</option>
-                </select>
-            </label>
-            <button onClick={handleSolveMazeClick}>Solucionar Labirinto</button>
+            <div className="controls-container">
+                <h3>Solucionar Labirinto</h3>
+                <label>
+                    Algoritmo:
+                    <select value={algorithm} onChange={(e) => setAlgorithm(e.target.value)}>
+                        <option value="bfs">BFS</option>
+                        <option value="dfs">DFS</option>
+                    </select>
+                </label>
+                <button onClick={handleSolveMazeClick}>Solucionar Labirinto</button>
+            </div>
         </div>
     );
 }
